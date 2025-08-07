7750de9a592c438312ffae6b8e3977c2c474cdfb
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-08-08 01:41:47 +0900

    リセットボタン追加・入力処理を追加・設定値にuseMemoを付与して不要な計算を防止

--- sewingTool-app/src/components/MainTool.tsx
+++ sewingTool-app/src/components/MainTool.tsx
@@ -1,17 +1,39 @@
 import { useMainToolsContext } from "../hooks/useContext";
 import ValueDisplay from "./ValueDisplay";
 import ArmorInfoList from "./ArmorInfoList";
+import { useMemo } from "react";
 
 export default function MainTool() {
-  const { armorName, values, setValues } = useMainToolsContext();
+  const {
+    armorName,
+    armorType,
+    values,
+    setValues,
+    nowValues,
+    setNowValues,
+    settingValues,
+    setSettingValues,
+  } = useMainToolsContext();
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (armorType == "") {
+      return alert("防具種類を選択してください");
+    }
+    if (armorName == "") {
+      return alert("防具名を選択してください");
+    }
 
-    // データを送信する処理（ここにAPI呼び出しなどを追加）
-    console.log("送信データ:", values);
+    // 残り値から入力値を引く
+    const newNowValues = [...nowValues];
+    values.forEach((value, index) => {
+      if (value !== null) {
+        newNowValues[index] = newNowValues[index] - value;
+      }
+    });
+    setNowValues(newNowValues);
 
-    // 値を空にする
+    // 入力値を空にする
     setValues([null, null, null, null, null, null, null, null, null]);
   };
 
@@ -22,14 +44,17 @@ export default function MainTool() {
     setValues(newValues);
   };
 
-  // 防具名ごとに設定値を更新する為の関数
-  const ValueSetting = (): (number | null)[] => {
-    const obj = ArmorInfoList.find((item) => item.name === armorName);
+  // 防具名ごとに設定値を更新する（防具名と防具種類が変わったら再計算）
+  const ValueSetting = useMemo((): (number | null)[] => {
+    const obj = ArmorInfoList.find(
+      (item) => item.name === armorName && item.Typeid === armorType
+    );
     if (obj) {
-      return obj.SettingValues;
+      setSettingValues(obj.SettingValues as number[]);
+      return settingValues;
     }
     return [null, null, null, null, null, null, null, null, null];
-  };
+  }, [armorName, armorType, settingValues, setSettingValues]);
 
   return (
     <div>
@@ -52,17 +77,53 @@ export default function MainTool() {
               </div>
             ))}
           </div>
-          <button
-            type="submit"
-            className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
-          >
-            更新
-          </button>
+          <div className="flex flex-row gap-4">
+            <button
+              type="submit"
+              className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+            >
+              入力
+            </button>
+            <button
+              type="button"
+              className="mt-4 bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600"
+              onClick={() => {
+                setNowValues(settingValues);
+                setValues([
+                  null,
+                  null,
+                  null,
+                  null,
+                  null,
+                  null,
+                  null,
+                  null,
+                  null,
+                ]);
+              }}
+            >
+              リセット
+            </button>
+          </div>
         </form>
 
         <div className="grid grid-cols-3 gap-4">
-          {ValueSetting().map((value, index) => (
-            <ValueDisplay key={index} value={value} label={`${index + 1}`} />
+          {ValueSetting.map((value, index) => (
+            <ValueDisplay
+              key={index}
+              value={value}
+              label={`設定値${index + 1}`}
+            />
+          ))}
+        </div>
+
+        <div className="grid grid-cols-3 gap-4">
+          {nowValues.map((value, index) => (
+            <ValueDisplay
+              key={index}
+              value={value}
+              label={`残り値${index + 1}`}
+            />
           ))}
         </div>
       </div>
