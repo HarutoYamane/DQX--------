a2faeb0df8a566762ffc2f073fb8cf73cd80afa5
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-08-08 09:10:42 +0900

    PowerRotateとClothIdの項目を追加

--- sewingTool-app/src/components/ArmorInfoList.tsx
+++ sewingTool-app/src/components/ArmorInfoList.tsx
@@ -1,64 +1,84 @@
-interface ArmorInfo {
-  Typeid: string; //防具種類
-  name: string; //防具名
-  SettingValues: (number | null)[]; // 最大値の配列(左上→中央上→右上→左中央→中央→右中央→左下→中央下→右下）
-}
+import type { ArmorInfo } from "../types/TodoType";
 
+// TypeId {head:頭 body_upper:体上 body_lower:体下 arm:腕 leg:脚}
+// ClothId {1:再生布 2:虹布 3:会心布 4:倍半布}
 const ArmorInfoList: ArmorInfo[] = [
   {
-    Typeid: "head",
-    name: "鋼の頭防具",
+    TypeId: "head",
+    Name: "鋼の頭防具",
     SettingValues: [10, 15, 20, 25, 30, 35, 40, 45, 50],
+    PowerRotate: ["強い", "弱い", "最強", "普通", "?", "弱い"],
+    ClothId: 1,
   },
   {
-    Typeid: "body_upper",
-    name: "鉄の体上防具",
+    TypeId: "body_upper",
+    Name: "鉄の体上防具",
     SettingValues: [20, 25, 30, 35, 40, 45, 50, 55, 60],
+    PowerRotate: ["強い", "弱い", "最強", "普通", "?", "弱い"],
+    ClothId: 2,
   },
   {
-    Typeid: "body_lower",
-    name: "鉄の体下防具",
+    TypeId: "body_lower",
+    Name: "鉄の体下防具",
     SettingValues: [30, 35, 40, 45, 50, 55, 60, 65, 70],
+    PowerRotate: ["強い", "弱い", "最強", "普通", "?", "弱い"],
+    ClothId: 2,
   },
   {
-    Typeid: "arm",
-    name: "金の腕防具",
+    TypeId: "arm",
+    Name: "金の腕防具",
     SettingValues: [40, 45, 50, 55, 60, 65, 70, 75, 80],
+    PowerRotate: ["強い", "弱い", "最強", "普通", "?", "弱い"],
+    ClothId: 4,
   },
   {
-    Typeid: "leg",
-    name: "プラチナの脚防具",
+    TypeId: "leg",
+    Name: "プラチナの脚防具",
     SettingValues: [15, 20, 25, 30, 35, 40, 45, 50, 55],
+    PowerRotate: ["強い", "弱い", "最強", "普通", "?", "弱い"],
+    ClothId: 2,
   },
   {
-    Typeid: "leg",
-    name: "鋼の脚防具",
+    TypeId: "leg",
+    Name: "鋼の脚防具",
     SettingValues: [25, 30, 35, 40, 45, 50, 55, 60, 65],
+    PowerRotate: ["強い", "弱い", "最強", "普通", "?", "弱い"],
+    ClothId: 1,
   },
   {
-    Typeid: "leg",
-    name: "銀の脚防具",
+    TypeId: "leg",
+    Name: "銀の脚防具",
     SettingValues: [35, 40, 45, 50, 55, 60, 65, 70, 75],
+    PowerRotate: ["強い", "弱い", "最強", "普通", "?", "弱い"],
+    ClothId: 3,
   },
   {
-    Typeid: "leg",
-    name: "金の脚防具",
+    TypeId: "leg",
+    Name: "金の脚防具",
     SettingValues: [45, 50, 55, 60, 65, 70, 75, 80, 85],
+    PowerRotate: ["強い", "弱い", "最強", "普通", "?", "弱い"],
+    ClothId: 4,
   },
   {
-    Typeid: "head",
-    name: "鉄の頭防具",
+    TypeId: "head",
+    Name: "鉄の頭防具",
     SettingValues: [8, 12, 16, 20, 24, 28, 32, 36, 40],
+    PowerRotate: ["強い", "弱い", "最強", "普通", "?", "弱い"],
+    ClothId: 2,
   },
   {
-    Typeid: "body_upper",
-    name: "鋼の体上防具",
+    TypeId: "body_upper",
+    Name: "鋼の体上防具",
     SettingValues: [16, 20, 24, 28, 32, 36, 40, 44, 48],
+    PowerRotate: ["強い", "弱い", "最強", "普通", "?", "弱い"],
+    ClothId: 3,
   },
   {
-    Typeid: "body_lower",
-    name: "銀の体下防具",
+    TypeId: "body_lower",
+    Name: "銀の体下防具",
     SettingValues: [24, 28, 32, 36, 40, 44, 48, 52, 56],
+    PowerRotate: ["強い", "弱い", "最強", "普通", "?", "弱い"],
+    ClothId: 4,
   },
 ];
 
