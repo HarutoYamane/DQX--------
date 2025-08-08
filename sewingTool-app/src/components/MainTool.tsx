1d448098d7122550d1cc0499c10c2a1f6555c5cf
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-08-08 09:13:28 +0900

    設定値表示をSelector画面で移動し、縫い強度の表示&処理を追加

--- sewingTool-app/src/components/MainTool.tsx
+++ sewingTool-app/src/components/MainTool.tsx
@@ -1,7 +1,9 @@
 import { useMainToolsContext } from "../hooks/useContext";
 import ValueDisplay from "./ValueDisplay";
-import ArmorInfoList from "./ArmorInfoList";
+import { LuCheck, LuInfo, LuArrowBigRight } from "react-icons/lu";
 import { useMemo } from "react";
+import ArmorInfoList from "../components/ArmorInfoList";
+import { ArmorObjIndex } from "../hooks/useTodo";
 
 export default function MainTool() {
   const {
@@ -12,7 +14,6 @@ export default function MainTool() {
     nowValues,
     setNowValues,
     settingValues,
-    setSettingValues,
   } = useMainToolsContext();
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -37,6 +38,27 @@ export default function MainTool() {
     setValues([null, null, null, null, null, null, null, null, null]);
   };
 
+  // 縫い強度のローテート順を取得
+  const PowerRotate = useMemo(
+    () => ArmorInfoList[ArmorObjIndex(armorName, armorType)]?.PowerRotate || [],
+    [armorName, armorType]
+  );
+
+  const PowerColor = (value: string) => {
+    switch (value) {
+      case "強い":
+        return "bg-orange-500 px-2 py-1";
+      case "弱い":
+        return "bg-blue-500 px-2 py-1";
+      case "普通":
+        return "bg-green-500 px-2 py-1";
+      case "最強":
+        return "bg-red-500 px-2 py-1";
+      case "?":
+        return "bg-gray-500 px-4 py-1";
+    }
+  };
+
   // 入力値を更新する為の関数
   const InputValue = (index: number, newValue: number) => {
     const newValues = [...values];
@@ -44,27 +66,27 @@ export default function MainTool() {
     setValues(newValues);
   };
 
-  // 防具名ごとに設定値を更新する（防具名と防具種類が変わったら再計算）
-  const ValueSetting = useMemo((): (number | null)[] => {
-    const obj = ArmorInfoList.find(
-      (item) => item.name === armorName && item.Typeid === armorType
-    );
-    if (obj) {
-      setSettingValues(obj.SettingValues as number[]);
-      return settingValues;
-    }
-    return [null, null, null, null, null, null, null, null, null];
-  }, [armorName, armorType, settingValues, setSettingValues]);
-
   return (
     <div>
       <h1>裁縫職人ツール</h1>
       <div>
         <h2>{armorName}</h2>
+        <div className="flex flex-row gap-2">
+          {PowerRotate.map((value, index) => (
+            <div key={index} className="flex flex-row items-center">
+              <div className={`${PowerColor(value)} text-white rounded-md`}>
+                {value}
+              </div>
+              {index < PowerRotate.length - 1 && (
+                <LuArrowBigRight className="mx-2 text-gray-700" size={24} />
+              )}
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="flex flex-row gap-4">
+      <div className="flex flex-row gap-8 mt-4">
         <form onSubmit={handleSubmit}>
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid grid-cols-3 gap-4 mb-4">
             {values.map((value, index) => (
               <div key={index} className="flex flex-col gap-2">
                 <label className="text-sm font-medium">値 {index + 1}</label>
@@ -78,44 +100,35 @@ export default function MainTool() {
             ))}
           </div>
           <div className="flex flex-row gap-4">
-            <button
-              type="submit"
-              className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
-            >
-              入力
-            </button>
-            <button
-              type="button"
-              className="mt-4 bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600"
-              onClick={() => {
-                setNowValues(settingValues);
-                setValues([
-                  null,
-                  null,
-                  null,
-                  null,
-                  null,
-                  null,
-                  null,
-                  null,
-                  null,
-                ]);
-              }}
-            >
-              リセット
-            </button>
+            <div className="flex flex-row items-center gap-2 mt-4 bg-blue-500 text-white px-4 py-2 rounded-md transition-all duration-300 hover:bg-blue-600">
+              <LuCheck size={24} color="white" />
+              <button type="submit">入力</button>
+            </div>
+
+            <div className="flex flex-row items-center gap-2 mt-4 bg-green-500 text-white px-4 py-2 rounded-md transition-all duration-300 hover:bg-green-600">
+              <LuInfo size={24} color="white" />
+              <button
+                type="button"
+                onClick={() => {
+                  setNowValues(settingValues.map((value) => value || 0));
+                  setValues([
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                  ]);
+                }}
+              >
+                リセット
+              </button>
+            </div>
           </div>
         </form>
-
-        <div className="grid grid-cols-3 gap-4">
-          {ValueSetting.map((value, index) => (
-            <ValueDisplay
-              key={index}
-              value={value}
-              label={`設定値${index + 1}`}
-            />
-          ))}
-        </div>
 
         <div className="grid grid-cols-3 gap-4">
           {nowValues.map((value, index) => (
