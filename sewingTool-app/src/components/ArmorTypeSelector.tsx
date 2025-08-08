fb75dc3e0b649048c7ca117f7fde286e6cc56f9c
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-08-08 09:12:15 +0900

    見た目の微調整のみ

--- sewingTool-app/src/components/ArmorTypeSelector.tsx
+++ sewingTool-app/src/components/ArmorTypeSelector.tsx
@@ -1,17 +1,19 @@
 import { useMainToolsContext } from "../hooks/useContext";
-import { Gi3dHammer } from "react-icons/gi";
+import { LuShirt, LuSearch } from "react-icons/lu";
 
-export default function WeaponTypeSelector() {
+export default function ArmorTypeSelector() {
   const { armorType, setArmorType } = useMainToolsContext();
 
   return (
     <form className="flex flex-col gap-4 ">
-      <div className="text-2xl font-bold">防具種類の選択</div>
+      <div className="flex flex-row items-center gap-2">
+        <LuShirt size={24} color="red" />
+        <div className="text-2xl font-bold">防具種類の選択</div>
+      </div>
       <div className="flex flex-col gap-2">
         <div className="flex flex-row items-center">
-          <div className="scale-x-[-1] rotate-[30deg]">
-            {/* 左右反転 + 20度回転 */}
-            <Gi3dHammer size={24} color="red" />
+          <div>
+            <LuSearch size={24} color="red" />
           </div>
           <label htmlFor="Type">防具種類</label>
         </div>
@@ -24,11 +26,11 @@ export default function WeaponTypeSelector() {
             className="border border-gray-300 rounded-md p-2"
           >
             <option value="">防具種類を選択してください</option>
-            <option value="head">頭防具</option>
-            <option value="body_upper">体上防具</option>
-            <option value="body_lower">体下防具</option>
-            <option value="arm">腕防具</option>
-            <option value="leg">脚防具</option>
+            <option value="head">アタマ - 防具</option>
+            <option value="body_upper">体上 - 防具</option>
+            <option value="body_lower">体下 - 防具</option>
+            <option value="arm">ウデ - 防具</option>
+            <option value="leg">足 - 防具</option>
           </select>
         </div>
       </div>
